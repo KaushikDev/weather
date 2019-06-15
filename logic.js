3a333c1ba40157484a319fec248e98dcb6c9047b
kaushikDev <piyush.kaushik02@gmail.com>
2019-06-15 15:59:14 +0530

    edited

--- logic.js
+++ logic.js
@@ -1,138 +1,134 @@
-var longi;
-var lati;
-var x = document.getElementById("position");
 var searchedLoc;
-var longitude;
-var latitude;
+var apiByPlaceName;
+
+var searchContainer = document.getElementById("searchContainer");
+var searchContainerWidth = searchContainer.clientWidth;
+
+var weatherContainer = document.getElementById("weatherContainer");
+var weatherContainerHeight = weatherContainer.clientHeight;
+var weatherContainerWidth = weatherContainer.clientWidth;
+
 
  $("document").ready(function(){
  
- //+++++++++++'Weather by coordinates' section begins here. Workflow : Search for coordinates using a suitable api, convert coordinates into location using reverse geocode api, find weather of the required location using the weather api+++++++++   
-
- /*
-   $("#useCurrent").on("click", function(){
-	x.innerHTML="";
-	var geoAPI="http://ip-api.com/json";
-	document.getElementById("clearBtn").disabled= false;
-	cleanSlate();
-		
-			$.getJSON(geoAPI, function(data){
-			longitude = data.lon;
-			console.log("longitude== "+longitude);
-			
-			longi = longitude.toFixed(6);
-			latitude = data.lat;
-			console.log("latitude== "+latitude);
-			lati = latitude.toFixed(6);
-			x.innerHTML = "Location identified as : "+lati+" , "+longi;
-			var geoCodeAPI = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lati+","+longi+"&key=AIzaSyCw2PTn1URdm2BPGU5daVOHDpav8KcqOOs" ;
-					$.getJSON(geoCodeAPI, function(geoData){
-						var placeName = geoData.results[0].address_components[4].long_name;
-						var apiByLatLong = "https://api.apixu.com/v1/forecast.json?key=c07a6d9c3d874f3cbd451147171606&q="+placeName;
-						
-								$.getJSON(apiByLatLong, function(data){
-									document.getElementById("displayIcon").src = "https:"+data.current.condition.icon;
-									document.getElementById("displayTime").innerHTML = 'Current Time   : '+data.location.localtime;
-									document.getElementById("displayCity").innerHTML = 'City           : '+data.location.name;
-									document.getElementById("displayCountry").innerHTML = 'Country        : '+data.location.country;
-									document.getElementById("displayTemperature").innerHTML = 'Temperature    : '+data.current.temp_c+'C / '+	data.current.temp_f+'F';
-									document.getElementById("displayCurrent").innerHTML = 'Current        : '+data.current.condition.text;
-									document.getElementById("displayForecast").innerHTML = 'Forecast       : '+data.forecast.forecastday[0].day.condition.text;
-									document.getElementById("displayHumidity").innerHTML = 'Humidity       : '+data.current.humidity+"%";
-									document.getElementById("displayWindSpeed").innerHTML = 'Wind Speed     : '+data.current.wind_mph+" mph / "+data.current.wind_kph+" kph" ;
-									document.getElementById("displayWindDirection").innerHTML = 'Wind Direction : '+data.current.wind_dir;
-								});
-					});
-   
-    
-			}).fail(function() { 
-				x.innerHTML = "Sorry, we are unable to track your current location right now. Please use 'Search a Location' feature.";
-				
-			});
-		
-  
-   
-   
-   });
-   
-   */
-//+++++++++++'Weather by coordinates' section ends here...
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++'Weather by searched location' section begins here. Workflow : Search for the required location using search box, find weather of the required location using the weather api+++++++++  
+ //+++++++++++'Weather by searched location' section begins here. Workflow : Search for the required location using search box, find weather of the required location using the weather api+++++++++  
    $("#searchBtn").on("click", function(){
-		x.innerHTML="";
-		cleanSlate();
-		document.getElementById("clearBtn").disabled= false;
-		//document.getElementById("showWeather").disabled= false;
+
+	
 		searchedLoc = document.getElementById("searchBox").value;
 		console.log(searchedLoc);
+		
+	
     
-		var apiByPlaceName="https://api.apixu.com/v1/forecast.json?key=c07a6d9c3d874f3cbd451147171606&q="+searchedLoc;
+		apiByPlaceName="https://api.apixu.com/v1/forecast.json?key=c07a6d9c3d874f3cbd451147171606&q="+searchedLoc;
     
 		$.getJSON(apiByPlaceName, function(data){
-			
-			document.getElementById("displayIcon").innerHTML="<img class='img-responsive' src='https:"+data.current.condition.icon+"' style='width:100px;height:100px;border-radius:50%;'>";
 			document.getElementById("displayIcon").src = "https:"+data.current.condition.icon;
-			document.getElementById("displayTime").innerHTML = 'Current Time   : '+data.location.localtime;
-			document.getElementById("displayCity").innerHTML = 'City           : '+data.location.name;
-			document.getElementById("displayCountry").innerHTML = 'Country        : '+data.location.country;
-			document.getElementById("displayTemperature").innerHTML = 'Temperature    : '+data.current.temp_c+'C / '+data.current.temp_f+'F';
-			document.getElementById("displayCurrent").innerHTML = 'Current        : '+data.current.condition.text;
-			document.getElementById("displayForecast").innerHTML = 'Forecast       : '+data.forecast.forecastday[0].day.condition.text;
-			document.getElementById("displayHumidity").innerHTML = 'Humidity       : '+data.current.humidity+"%";
-			document.getElementById("displayWindSpeed").innerHTML = 'Wind Speed     : '+data.current.wind_mph+" mph / "+data.current.wind_kph+" kph" ;
+			document.getElementById("displayTime").innerHTML = 'Current Time : '+data.location.localtime;
+			document.getElementById("displayCity").innerHTML = data.location.name;
+			document.getElementById("displayCountry").innerHTML = data.location.country;
+			document.getElementById("displayTemperature").innerHTML = data.current.temp_c+'&#8451;';
+			document.getElementById("displayCurrent").innerHTML = data.current.condition.text;
+			document.getElementById("displayForecast").innerHTML = 'Forecast : '+data.forecast.forecastday[0].day.condition.text;
+			document.getElementById("displayHumidity").innerHTML = 'Humidity : '+data.current.humidity+"%";
+			document.getElementById("displayWindSpeed").innerHTML = 'Wind Speed : '+data.current.wind_kph+" kph" ;
 			document.getElementById("displayWindDirection").innerHTML = 'Wind Direction : '+data.current.wind_dir;
 			
-		
+			setWeatherBackground(data.current.condition.text);
+			setSearchContainerPosition();
+			setWeatherContainerPosition();
 		}).fail(function() { 
-			x.innerHTML = "Oops, we couldn't find this place. Please try with a valid 'CITY' name. Thanks.";
-		
+			alert("Oops, we couldn't find this place. Please try with a valid 'CITY' name. Thanks.");
+			weatherContainer.style.visibility = "hidden";
 		});
 	 	 
    });
+
+});
+
+
+function setWeatherContainerPosition(){
+	weatherContainer.style.visibility = "visible";
+	weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
+	weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.3}px)`;
+}
+
+function setSearchContainerPosition(){
+	searchContainer.style.top = 0;
+	
+}
+
+
+function setWeatherBackground(weatherCondition){
+	switch (weatherCondition) {
+		case "Sunny":
+			document.body.style.backgroundImage = 'url("images/sunny-min.jpg")';
+			break;
+		case "Clear":
+			document.body.style.backgroundImage = 'url("images/clear-min.jpg")';
+			break;
+		case "Partly cloudy":
+			document.body.style.backgroundImage = 'url("images/partlycloudy-min.jpg")';
+			break;
+		case "Cloudy":
+			document.body.style.backgroundImage = 'url("images/cloudy-min.jpg")';
+			break;
+		case "Overcast":
+			document.body.style.backgroundImage = 'url("images/overcast-min.jpg")';
+			break;
+		case "Mist":
+			document.body.style.backgroundImage = 'url("images/fog-min.jpg")';
+			break;
+		case "Patchy rain possible":
+		case "Patchy freezing drizzle possible":
+		case "Patchy light drizzle":
+		case "Light drizzle":
+		case "Freezing drizzle":
+		case "Heavy freezing drizzle":
+		case "Patchy light rain":
+		case "Light rain":
+		case "Moderate rain at times":
+		case "Moderate rain":
+		case "Patchy light rain with thunder":
+		case "Moderate or heavy rain with thunder":
+			document.body.style.backgroundImage = 'url("images/drizzle-min.jpg")';
+			break;
+		case "Moderate or heavy rain shower":	
+		case "Heavy rain at times":
+		case "Heavy rain":
+		case "Torrential rain shower":
+			document.body.style.backgroundImage = 'url("images/rain-min.jpg")';
+			break;
+		case "Patchy light snow":
+		case "Light snow":
+		case "Patchy moderate snow":
+		case "Moderate snow":
+		case "Light snow showers":
+			document.body.style.backgroundImage = 'url("images/snow-min.jpg")';
+			break;
+		case "Patchy heavy snow":
+		case "Heavy snow":
+			document.body.style.backgroundImage = 'url("images/heavysnow-min.jpg")';
+			break;
+		case "Ice pellets":
+		case "Light showers of ice pellets":
+		case "Moderate or heavy showers of ice pellets":
+			document.body.style.backgroundImage = 'url("images/hail-min.jpg")';
+			break;
+		case "Patchy light rain with thunder":
+		case "Moderate or heavy rain with thunder":
+		case "Patchy light snow with thunder":
+		case "Moderate or heavy snow with thunder":
+			document.body.style.backgroundImage = 'url("images/thunderstorm-min.jpg")';
+			break;
+		case "Fog":
+		case "Freezing fog":
+			document.body.style.backgroundImage = 'url("images/fog-min.jpg")';
+			break;
+
+		default:
+			document.body.style.backgroundImage = 'url("images/default.jpg")';
+			break;
+	}
+}
    
- });
-   
-
-  
-function init(){
-document.getElementById("searchManual").style.visibility="hidden";
-document.getElementById("clearBtn").disabled= true;
-}
-
-function showHide(){
-if(document.getElementById("useManual").innerHTML==='<i class="fa fa-globe" aria-hidden="true"></i> Search a Location'){
-clean();
-document.getElementById("useManual").innerHTML='<i class="fa fa-globe" aria-hidden="true"></i> Close Manual Search';
-document.getElementById("searchManual").style.visibility="visible";
-}
-else if(document.getElementById("useManual").innerHTML==='<i class="fa fa-globe" aria-hidden="true"></i> Close Manual Search'){
-document.getElementById("useManual").innerHTML='<i class="fa fa-globe" aria-hidden="true"></i> Search a Location';
-clean();
-document.getElementById("searchManual").style.visibility="hidden";
-}
-}
-
-function clean(){
-document.getElementById("clearBtn").disabled= true;
-//document.getElementById("showWeather").disabled= true;
-document.getElementById("searchBox").value="";
-document.getElementById("position").innerHTML="";
-cleanSlate();
-lati='';
-longi='';
-}
-
-function cleanSlate(){
-document.getElementById("displayIcon").innerHTML="<img class='img-responsive' src='spot_weather.jpg' style='width:200px;height:200px;border-radius:50%;'>";
-document.getElementById("displayTime").innerHTML ="";
-document.getElementById("displayCity").innerHTML="";
-document.getElementById("displayCountry").innerHTML="";
-document.getElementById("displayTemperature").innerHTML="";
-document.getElementById("displayCurrent").innerHTML="";
-document.getElementById("displayForecast").innerHTML="";
-document.getElementById("displayHumidity").innerHTML="";
-document.getElementById("displayWindSpeed").innerHTML="";
-document.getElementById("displayWindDirection").innerHTML="";
-}
