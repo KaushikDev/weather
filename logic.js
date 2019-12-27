aaf61e1684d4723a85ca5f390c685d1bb525b240
Piyush Kaushik <piyush.kaushik02@gmail.com>
2019-12-27 21:28:19 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -38,7 +38,7 @@ function onPermissionGranted(position){
 	var lon = position.coords.longitude;
 	console.log(lat, lon);
 
-	apiByPlaceName="https://api.apixu.com/v1/forecast.json?key=c07a6d9c3d874f3cbd451147171606&q="+lat+","+lon+"&days=5";
+	apiByPlaceName="https://crossorigin.me/https://api.apixu.com/v1/forecast.json?key=c07a6d9c3d874f3cbd451147171606&q="+lat+","+lon+"&days=5";
     
 	$.getJSON(apiByPlaceName, function(data){
 		displayIcon.src = "https:"+data.current.condition.icon;
@@ -89,7 +89,7 @@ $("document").ready(function(){
    $("#searchBtn").on("click", function(){
 
 		searchedLoc = document.getElementById("searchBox").value;
-		apiByPlaceName="https://api.apixu.com/v1/forecast.json?key=c07a6d9c3d874f3cbd451147171606&q="+searchedLoc+"&days=5";
+		apiByPlaceName="https://crossorigin.me/https://api.apixu.com/v1/forecast.json?key=c07a6d9c3d874f3cbd451147171606&q="+searchedLoc+"&days=5";
     
 		$.getJSON(apiByPlaceName, function(data){
 			displayIcon.src = "https:"+data.current.condition.icon;
